5ef3c88f8d9e3d27cca4c708707966d53bcfbd40
JD <jdevore@josephdevore.com>
2018-05-11 23:36:30 -0700

    Fizzbuzz and maxChar solutions

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,28 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    // loop from 1 to n
+    for(let i = 1; i <= n; i++)
+    {
+        // if mod 5 and mod 3, print fizzbuzz
+        if(i % 5 === 0 && i % 3 === 0) {
+            console.log("fizzbuzz");
+
+        // if mod 3, print fizz
+        } else if(i % 3 === 0 ) {
+            console.log("fizz");
+        
+        // if mod 5, print buzz
+        } else if(i % 5 === 0 ) {
+            console.log("buzz");
+            
+        // if not mod 3 and not mod 5, print i
+        } else {
+            console.log(i);
+        }
+    }
+
+}
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,44 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let mostUsed = '';
+    let mostUsedCount = 0;
+
+    // loop over string of characters
+    for(let char of str)
+    {
+        // if this char is defined increment
+        if(charMap[char])
+        {
+            // increment char count
+            charMap[char]++;
+        } else {
+            // set char count to 1
+            charMap[char] = 1;
+        }
+
+        // or you could do this instead of if statement
+        // charMap[char] = charMap[char] +1 || 1;
+    }
+
+    // loop over character map object
+    for(let char in charMap)
+    {
+        // if this iteration's char count is greater than the max
+        if(charMap[char] > mostUsedCount)
+        {
+            // set this iteration's char as the winner of most used
+            mostUsed = char;
+
+            // get the char count value
+            mostUsedCount = charMap[char];
+        }
+    }
+
+    return mostUsed;
+}
 
 module.exports = maxChar;
+ 
