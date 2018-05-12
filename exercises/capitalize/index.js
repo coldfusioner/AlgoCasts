8aec9c8790bbada754881ad086e0783bd5b84391
JD <jdevore@josephdevore.com>
2018-05-12 14:40:35 -0700

    Solution for capitalizing first letter of each word in a string

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    // split words at space
+    const words = str.split(' ');
+
+    // array to hold capitalized words
+    const capitalized = [];
+
+    // loop over words
+    for(let word of words) {
+        // capitalize first let of the word and add to array
+        capitalized.push(word.substr(0, 1).toUpperCase() + word.substr(1));
+    }
+    // join the words back together separated by a space
+    return capitalized.join(' ');
+}
 
 module.exports = capitalize;
